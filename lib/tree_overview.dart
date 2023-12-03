502e471fc40b7e1ac4c8b50fb41b4f8067ba6979
LeonardHerbst <leonard.herbst@tu-dresden.de>
2023-12-03 21:48:29 +0100

    Started creating widget for the tree overview

--- lib/tree_overview.dart
+++ lib/tree_overview.dart
@@ -9,20 +9,49 @@ class TreeVisualizer extends StatelessWidget {
   TreeVisualizer({required this.tree});
 
   Widget _buildTree(TreeNode node) {
-    return Column(
-      children: [
-        Text(node.name),
-        if (node.children.isNotEmpty)
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: node.children.map((child) => _buildTree(child)).toList(),
+    return Container(
+      padding: EdgeInsets.only(left: 32, right: 32, top: 4, bottom: 4),
+      margin: EdgeInsets.symmetric(vertical: 4.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.lightBlueAccent, width: 2.0),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 2.0),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(node.name, style: const TextStyle(fontWeight: FontWeight.bold))
             ),
           ),
-      ],
+          if (node.children.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: node.children.map((child) => _buildTree(child)).toList(),
+              ),
+            ),
+        ],
+      ),
     );
   }
+
+  // Text(node.name, style: TextStyle(fontWeight: FontWeight.bold)),
+  // if (node.children.isNotEmpty)
+  //   Padding(
+  //     padding: const EdgeInsets.only(left: 16.0),
+  //     child: Column(
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: node.children.map((child) => _buildTree(child)).toList(),
+  //     ),
+  //   ),
 
   @override
   Widget build(BuildContext context) {
