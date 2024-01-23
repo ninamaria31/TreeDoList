3e7c7b3e568dc093bdcac3854a62f82b89c9db79
Nina <nina@ninoma.de>
2024-01-23 14:43:31 +0100

    DB http connections, update db in Create, Update, Delete

--- lib/tree/tree_callbacks.dart
+++ lib/tree/tree_callbacks.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:tree_do/tree/tree.dart';
 import 'package:intl/intl.dart';
+import '../services/flask_server.dart';
 
 mixin TreeCallbacks<T extends StatefulWidget> on State<T> {
   void onTapCallback(TreeNode node, BuildContext context) =>
@@ -19,7 +19,6 @@ mixin TreeCallbacks<T extends StatefulWidget> on State<T> {
   void showDetails(TreeNode node, BuildContext context, {bool? edit}) {
     // remove: just call node.removeSelf() on the node which is supposed to be removed
     // if the node or tree was modified call node.notifyModification() or tree.modify() (the first just calls the latter)
-
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -48,6 +47,7 @@ mixin TreeCallbacks<T extends StatefulWidget> on State<T> {
                       ),
                       child: const Text('edit'),
                       onPressed: () {
+                        Navigator.of(context).pop();
                         showEditDialog(context, node);
                       },
                     ),
@@ -62,6 +62,7 @@ mixin TreeCallbacks<T extends StatefulWidget> on State<T> {
                       ),
                       child: const Text('delete'),
                       onPressed: () {
+                        Navigator.of(context).pop();
                         showDialog(
                           context: context,
                           builder: (BuildContext context) {
@@ -79,8 +80,12 @@ mixin TreeCallbacks<T extends StatefulWidget> on State<T> {
                                 TextButton(
                                   child: const Text('yes'),
                                   onPressed: () {
-                                    node.removeSelfFromParent();
-                                    // Put your code for removing the node here
+                                    node.notifyModification();
+                                    node.removeSelf();
+                                    setState(() {
+                                      _rebuildBitonicChildren = true;
+                                    });
+                                    updateTreeDB(node.tree?.toJson());
                                     Navigator.of(context).pop();
                                     showDialog(
                                       context: context,
@@ -219,9 +224,11 @@ mixin TreeCallbacks<T extends StatefulWidget> on State<T> {
                 child: const Text('Save'),
                 onPressed: () {
                   // Save the changes here
+                  node.notifyModification();
                   node.name = updName;
                   node.description = updDescription;
                   node.dueDate = updDuedate;
+                  updateTreeDB(node.tree?.toJson());
                   Navigator.of(context).pop();
                 },
               ),
@@ -232,20 +239,146 @@ mixin TreeCallbacks<T extends StatefulWidget> on State<T> {
     );
   }
 
-  void toggleComplete(TreeNode node) {
+  void toggleComplete(TreeNode node) async {
+    (node.completed == null) ? node.complete() : node.undoComplete();
+    Tree? updatedTree = await updateTreeDB(node.tree?.toJson()); // updates the tree in the database
     setState(() {
-      (node.completed == null) ? node.complete() : node.undoComplete();
+      node;
     });
-    // completion: call complete() and undoComplete() in the TreeNode directly
   }
 
-  void addChild(TreeNode node, BuildContext context) {
-    setState(() {
-      TreeNode newNode = TreeNode("", Priority.medium);
-      node.addChild(newNode);
-      _rebuildBitonicChildren = true;
-      showDetails(newNode, context, edit: true);
-    });
+  void addChild(TreeNode node, BuildContext context) async {
+    TreeNode newNode = TreeNode("", Priority.medium);
+    showAddNodeDialog(context, node, newNode);
+  }
+
+  void showAddNodeDialog(BuildContext context, TreeNode node, TreeNode newNode) {
+    var name;
+    var description;
+    var dueDate;
+
+    final dueDateController =
+    TextEditingController(text: formatDate(node.dueDate));
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width *
+                0.05, // 5% from left and right
+            vertical: MediaQuery.of(context).size.height *
+                0.05, // 10% from top and bottom
+          ),
+          child: AlertDialog(
+            contentPadding: EdgeInsets.zero,
+            // Remove padding inside the dialog
+            insetPadding: EdgeInsets.zero,
+            // Remove padding around the dialog
+            clipBehavior: Clip.none,
+            title: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    newNode.removeSelf();
+                    setState(() {
+                      _rebuildBitonicChildren = true;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                const Text(
+                  "Add a new task",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            content: SizedBox(
+              width: MediaQuery.of(context).size.width *
+                  0.9, // 90% of screen width
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    // Add padding around the TextFormField
+                    child: TextFormField(
+                      initialValue: null,
+                      decoration: const InputDecoration(
+                        labelText: 'Title',
+                      ),
+                      onChanged: (value) {
+                        name = value;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    // Add padding around the TextFormField
+                    child: TextFormField(
+                      initialValue: null,
+                      decoration: const InputDecoration(
+                        labelText: 'Description',
+                      ),
+                      onChanged: (value) {
+                        description = value;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    // Add padding around the TextFormField
+                    child: TextFormField(
+                      readOnly: true,
+                      // make this field read-only
+                      decoration: const InputDecoration(
+                        labelText: 'Due Date',
+                      ),
+                      controller: dueDateController,
+                      // use the TextEditingController
+                      onTap: () async {
+                        // show the date picker when the due date field is tapped
+                        final selectedDate = await showDatePicker(
+                          context: context,
+                          initialDate: null ?? DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2100),
+                        );
+
+                        if (selectedDate != null) {
+                          dueDate = selectedDate;
+                          // Update the text of the TextFormField
+                          dueDateController.text = formatDate(selectedDate);
+                        }
+                      },
+                    ),
+                  ),
+                  // Add more fields for other properties
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                child: const Text('Save'),
+                onPressed: () {
+                  // Save the changes here
+                  newNode.name = name;
+                  newNode.description = description;
+                  newNode.dueDate = dueDate;
+                  newNode.notifyModification();
+                  setState(() {
+                    node.addChild(newNode);
+                    _rebuildBitonicChildren = true;
+                  });
+                  updateTreeDB(newNode.tree?.toJson());
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
   
   bool get rebuildBitonicChildren {
@@ -256,22 +389,26 @@ mixin TreeCallbacks<T extends StatefulWidget> on State<T> {
 
   String formatDate(DateTime? date) {
     var dayFormat = NumberFormat('#', 'en_US');
-    var dayString = dayFormat.format(date?.day);
-    var day = int.parse(dayString);
-    var suffix = 'th';
+    if (date == null) {
+      return 'No Due Date';
+    } else {
+      var dayString = dayFormat.format(date?.day);
+      var day = int.parse(dayString);
+      var suffix = 'th';
 
-    if (!(day >= 11 && day <= 13)) {
-      var lastDigit = day % 10;
-      if (lastDigit == 1) {
-        suffix = 'st';
-      } else if (lastDigit == 2) {
-        suffix = 'nd';
-      } else if (lastDigit == 3) {
-        suffix = 'rd';
+      if (!(day >= 11 && day <= 13)) {
+        var lastDigit = day % 10;
+        if (lastDigit == 1) {
+          suffix = 'st';
+        } else if (lastDigit == 2) {
+          suffix = 'nd';
+        } else if (lastDigit == 3) {
+          suffix = 'rd';
+        }
       }
-    }
 
-    var formatter = DateFormat('d\'$suffix\' MMMM yyyy');
-    return formatter.format(date!);
+      var formatter = DateFormat('d\'$suffix\' MMMM yyyy');
+      return formatter.format(date!);
+    }
   }
 }
